ce63dc9cb43b5c6fbb2fe3a789b44deb94acdbdc
Igor Nikolaev <Bujhm_@mail.ru>
2020-06-24 18:35:51 +0300

    Исправлены ошибки. Попытка номер два

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-let openbutton=document.querySelector('.profile__edit-button')
-let closebutton=document.querySelector('.popup__close-button')
-let popup=document.querySelector('.popup')
+let openbutton = document.querySelector('.profile__edit-button');
+let closebutton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let InputName = document.querySelector('.popup__input-name');
+let InputDiscription = document.querySelector('.popup__input-discription');
+let profileName = document.querySelector('.profile__name');
+let profileDiscription = document.querySelector('.profile__discription');
 
 function open(){
   popup.classList.add('popup_opened');
-  console.log(popup.classList)
+  InputName.value = profileName.textContent;
+  InputDiscription.value = profileDiscription.textContent;
 }
 
 function close(){
-  popup.classList.remove('popup_opened');   
+  popup.classList.remove('popup_opened');
+  
 }
 
 openbutton.addEventListener('click', open);
 closebutton.addEventListener('click', close);
 
-let formElement=document.querySelector('.popup__form')
+let formElement=document.querySelector('.popup__form');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let Input =document.querySelectorAll('.popup__input');
-    let name=Input[0].value;
-    let discription=Input[1].value;
-    let profileName=document.querySelector('.profile__name');
-    let profileDiscription=document.querySelector('.profile__discription');
-    profileName.textContent=name;
-    profileDiscription.textContent=discription;
+    
+    let name=InputName.value;
+    let discription = InputDiscription.value;
+    let profileName = document.querySelector('.profile__name');
+    let profileDiscription = document.querySelector('.profile__discription');
+    profileName.textContent = name;
+    profileDiscription.textContent = discription;
     close();
 }  
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+console.log(InputName.value);
